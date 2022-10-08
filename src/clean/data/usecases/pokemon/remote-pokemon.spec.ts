1cb8088056cc068bdeb52771bf71f08c9ccd975b
jeziellopes <jeziellopes@gmail.com>
2022-10-08 16:31:32 -0300

    refactor: move sut to factory

--- src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
+++ src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
@@ -2,13 +2,23 @@ import { HttpGetClientSpy } from '@/data/test/mock-http-client'
 import { RemotePokemon } from '@/data/usecases/pokemon/remote-pokemon'
 import { describe, expect, test } from '@jest/globals'
 
+type SutTypes = {
+  sut: RemotePokemon
+  httpGetClientSpy: HttpGetClientSpy
+}
+
+const makeSut = (url = 'any_url'): SutTypes => {
+  const httpGetClientSpy = new HttpGetClientSpy()
+  const sut = new RemotePokemon(url, httpGetClientSpy)
+  return { sut, httpGetClientSpy }
+}
+
 describe('RemotePokemon', () => {
   test('Should call HttpClient with correct URL', async () => {
-    const url = 'any_url'
-    const httpClientSpy = new HttpGetClientSpy()
-    const sut = new RemotePokemon(url, httpClientSpy)
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load()
 
-    expect(httpClientSpy.url).toBe(url)
+    expect(httpGetClientSpy.url).toBe(url)
   })
 })
