989634bc9c9f7a9833174b3959632ad56964c4a0
jeziellopes <jeziellopes@gmail.com>
2022-10-08 16:22:40 -0300

    test: improve remote pokemon tests

--- src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
+++ src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
@@ -1,19 +1,9 @@
-import { HttpGetClient } from '@/data/protocols/http/http-get-client'
+import { HttpGetClientSpy } from '@/data/test/mock-http-client'
+import { RemotePokemon } from '@/data/usecases/pokemon/remote-pokemon'
 import { describe, expect, test } from '@jest/globals'
-
-import { RemotePokemon } from './remote-pokemon'
 
 describe('RemotePokemon', () => {
   test('Should call HttpClient with correct URL', async () => {
-    class HttpGetClientSpy implements HttpGetClient {
-      url?: string
-
-      async get(url: string): Promise<void> {
-        this.url = url
-        return Promise.resolve()
-      }
-    }
-
     const url = 'any_url'
     const httpClientSpy = new HttpGetClientSpy()
     const sut = new RemotePokemon(url, httpClientSpy)
