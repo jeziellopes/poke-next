98962d47f064b84fffe02eb12e0aa8d52de2b379
jeziellopes <jeziellopes@gmail.com>
2022-10-08 20:11:34 -0300

    test: added new http client tests

--- src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
+++ src/clean/data/usecases/pokemon/remote-pokemon.spec.ts
@@ -1,5 +1,8 @@
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { HttpGetClientSpy } from '@/data/test/mock-http-client'
 import { RemotePokemon } from '@/data/usecases/pokemon/remote-pokemon'
+import { InternalServerError } from '@/domain/errors/internal-server-error'
+import { UnexpectedError } from '@/domain/errors/unexpected-error'
 import { describe, expect, test } from '@jest/globals'
 
 type SutTypes = {
@@ -18,7 +21,26 @@ describe('RemotePokemon', () => {
     const url = 'other_url'
     const { sut, httpGetClientSpy } = makeSut(url)
     await sut.load()
-
     expect(httpGetClientSpy.url).toBe(url)
+  })
+
+  test('Should throw InternalServerError when HttpGetClient return statusCode 500', async () => {
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.server_error,
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new InternalServerError())
+  })
+
+  test('Should throw UnexpectedError when HttpGetClient return statusCode 400', async () => {
+    const url = 'other_url'
+    const { sut, httpGetClientSpy } = makeSut(url)
+    httpGetClientSpy.response = {
+      statusCode: HttpStatusCode.badRequest,
+    }
+    const promise = sut.load()
+    await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 })
