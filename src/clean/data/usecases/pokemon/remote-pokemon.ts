a71b37e2020f9a1ce067cc31dfc8681cfab52f04
jeziellopes <jeziellopes@gmail.com>
2022-10-09 07:17:36 -0300

    refactor: data remote pokemon use case

--- src/clean/data/usecases/pokemon/remote-pokemon.ts
+++ src/clean/data/usecases/pokemon/remote-pokemon.ts
@@ -1,23 +1,22 @@
 import { HttpGetClient } from '@/data/protocols/http/http-get-client'
-import {
-  HttpStatusCode,
-  HttpResponse,
-} from '@/data/protocols/http/http-response'
+import { HttpStatusCode } from '@/data/protocols/http/http-response'
 import { InternalServerError } from '@/domain/errors/internal-server-error'
 import { UnexpectedError } from '@/domain/errors/unexpected-error'
+import { PokemonModel } from '@/domain/models/pokemon-model'
+import { Pokemon } from '@/domain/usecases/pokemon'
 
-export class RemotePokemon {
+export class RemotePokemon implements Pokemon {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient
+    private readonly httpGetClient: HttpGetClient<PokemonModel[]>
   ) {}
 
-  async load(): Promise<HttpResponse> {
+  async load(): Promise<PokemonModel[]> {
     const httpResponse = await this.httpGetClient.get(this.url)
 
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        return Promise.resolve(httpResponse)
+        return httpResponse.body
       case HttpStatusCode.server_error:
         throw new InternalServerError()
       default:
