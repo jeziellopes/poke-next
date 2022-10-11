d48eaa14506be805fb9ac7d9281e85b0dfb7ae25
jeziellopes <jeziellopes@gmail.com>
2022-10-11 14:54:05 -0300

    fix: api data interfaces exports

--- src/api/data/interfaces/index.ts
+++ src/api/data/interfaces/index.ts
@@ -1,3 +1,2 @@
 export * from '@/api/data/interfaces/repositories/pokemons-loader-repository'
 export * from '@/api/data/interfaces/data-sources/pokemons-loader-data-source'
-export * from '@/api/data/interfaces/env/env'
