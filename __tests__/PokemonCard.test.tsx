add3822129363b2d6a9607ae10dd8b9093cc62b3
jeziellopes <jeziellopes@gmail.com>
2022-10-13 13:35:42 -0300

    test: update card tests

--- __tests__/PokemonCard.test.tsx
+++ __tests__/PokemonCard.test.tsx
@@ -1,4 +1,4 @@
-import { Card } from '@/presentation/components/structure'
+import { PokemonCard } from '@/presentation/components/context'
 import { ThemeProvider } from '@/presentation/providers'
 import { render } from '@testing-library/react'
 import React from 'react'
@@ -7,7 +7,7 @@ describe('Card', () => {
   it('renders a card with title and image alt', () => {
     const { getByRole, getByAltText } = render(
       <ThemeProvider>
-        <Card
+        <PokemonCard
           title="Card Title"
           imageUrl="https://raw.githubusercontent.com/HybridShivam/Pokemon/master/assets/images/001.png"
         />
