cdc4b9fa8c132644c6307befdea2a770a4f02f11
jeziellopes <jeziellopes@gmail.com>
2022-10-13 11:39:07 -0300

    feat: added endpoints config helper

--- src/app/main/config/endpoints.ts
+++ src/app/main/config/endpoints.ts
@@ -1,6 +1,6 @@
 export const getEndpoint = (key: string): string => {
   const endpoints = {
-    pokemons: 'pokemon?offset=0&limit=300',
+    pokemons: 'pokemon?offset=0&limit=15',
   }
 
   return endpoints[key]
