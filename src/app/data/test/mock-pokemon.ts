7b97d6d0523ecd4c99c818e37c29780299d8a761
jeziellopes <jeziellopes@gmail.com>
2022-10-13 22:08:52 -0300

    test: add pokemons details tests and improvements

--- src/app/data/test/mock-pokemon.ts
+++ src/app/data/test/mock-pokemon.ts
@@ -1,8 +1,95 @@
-import { PokemonModel } from '@/domain/models'
+import { PokemonResponseModel } from '@/domain/models'
 
-export const mockPokemonResponse = (): PokemonModel[] => [
-  {
-    name: 'bulbasaur',
-    url: 'https://pokeapi.co/api/v2/pokemon/1/',
-  },
-]
+export const mockPokemonResponse = (): PokemonResponseModel => ({
+  count: 1154,
+  next: 'https://pokeapi.co/api/v2/pokemon?offset=15&limit=15',
+  previous: null,
+  results: [
+    {
+      name: 'bulbasaur',
+      url: 'https://pokeapi.co/api/v2/pokemon/1/',
+    },
+  ],
+})
+
+export const mockPokemonDetailsResponse = (): any => ({
+  id: 1,
+  name: 'bulbasaur',
+  url: 'any_url/1',
+  abilities: [
+    {
+      ability: {
+        name: 'overgrow',
+        url: 'https://pokeapi.co/api/v2/ability/65/',
+      },
+      is_hidden: false,
+      slot: 1,
+    },
+    {
+      ability: {
+        name: 'chlorophyll',
+        url: 'https://pokeapi.co/api/v2/ability/34/',
+      },
+      is_hidden: true,
+      slot: 3,
+    },
+  ],
+  base_experience: 64,
+  height: 69,
+  stats: [
+    {
+      base_stat: 45,
+      effort: 0,
+      stat: {
+        name: 'hp',
+        url: 'https://pokeapi.co/api/v2/stat/1/',
+      },
+    },
+    {
+      base_stat: 49,
+      effort: 0,
+      stat: {
+        name: 'attack',
+        url: 'https://pokeapi.co/api/v2/stat/2/',
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: 'grass',
+        url: 'https://pokeapi.co/api/v2/type/12/',
+      },
+    },
+    {
+      slot: 2,
+      type: {
+        name: 'poison',
+        url: 'https://pokeapi.co/api/v2/type/4/',
+      },
+    },
+  ],
+  weight: 69,
+})
+
+export const mockPokemonDetailsResult = () => ({
+  id: 1,
+  name: 'bulbasaur',
+  url: 'any_url/1',
+  abilities: ['overgrow', 'chlorophyll'],
+  base_experience: 64,
+  height: 69,
+  stats: [
+    {
+      base_stat: 45,
+      name: 'hp',
+    },
+    {
+      base_stat: 49,
+      name: 'attack',
+    },
+  ],
+  types: ['grass', 'poison'],
+  weight: 69,
+})
--- src/app/data/usecases/pokemon/remote-pokemon-details.spec.ts
+++ src/app/data/usecases/pokemon/remote-pokemon-details.spec.ts
@@ -1,30 +1,34 @@
 import { HttpStatusCode } from '@/data/protocols/http'
-import { HttpGetClientSpy, mockPokemonResponse } from '@/data/test'
-import { RemotePokemon } from '@/data/usecases'
+import {
+  HttpGetClientSpy,
+  mockPokemonDetailsResponse,
+  mockPokemonDetailsResult,
+} from '@/data/test'
+import { RemotePokemonDetails } from '@/data/usecases'
 import { InternalServerError, UnexpectedError } from '@/domain/errors'
-import { PokemonResponseModel } from '@/domain/models'
-import { PokemonParams } from '@/domain/usecases'
+import { PokemonDetailsModel } from '@/domain/models'
+import { PokemonDetailsParams } from '@/domain/usecases'
 import { describe, expect, test } from '@jest/globals'
 
 type SutTypes = {
-  sut: RemotePokemon
-  httpGetClientSpy: HttpGetClientSpy<PokemonResponseModel>
-  pokemonLoadParams: PokemonParams
+  sut: RemotePokemonDetails
+  httpGetClientSpy: HttpGetClientSpy<PokemonDetailsModel>
+  pokemonLoadParams: PokemonDetailsParams
 }
 
 const makeSut = (url = 'any_url'): SutTypes => {
-  const httpGetClientSpy = new HttpGetClientSpy<PokemonResponseModel>()
-  const pokemonLoadParams = { offset: '10', limit: '15' }
-  const sut = new RemotePokemon(url, httpGetClientSpy)
+  const httpGetClientSpy = new HttpGetClientSpy<PokemonDetailsModel>()
+  const pokemonLoadParams = { id: '1', name: 'bulbasaur' }
+  const sut = new RemotePokemonDetails(url, httpGetClientSpy)
   return { sut, httpGetClientSpy, pokemonLoadParams }
 }
 
-describe('RemotePokemon', () => {
+describe('RemotePokemonDetails', () => {
   test('Should call HttpClient with correct URL', async () => {
     const url = 'other_url'
     const { sut, httpGetClientSpy, pokemonLoadParams } = makeSut(url)
     await sut.load(pokemonLoadParams)
-    expect(httpGetClientSpy.url).toBe(`${url}?offset=10&limit=15`)
+    expect(httpGetClientSpy.url).toBe(`${url}/1`)
   })
 
   test('Should throw InternalServerError when HttpGetClient return statusCode 500', async () => {
@@ -47,14 +51,15 @@ describe('RemotePokemon', () => {
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
 
-  test('Should return PokemonModel when HttpGetClient gets statusCode 200', async () => {
+  test('Should return PokemonDetailsModel when HttpGetClient gets statusCode 200', async () => {
     const { sut, httpGetClientSpy, pokemonLoadParams } = makeSut()
-    const httpResponse = mockPokemonResponse()
+    const httpResponse = mockPokemonDetailsResponse()
+    const httpResult = mockPokemonDetailsResult()
     httpGetClientSpy.response = {
       statusCode: HttpStatusCode.ok,
       body: httpResponse,
     }
     const response = await sut.load(pokemonLoadParams)
-    expect(response).toEqual(httpResponse)
+    expect(response).toEqual(httpResult)
   })
 })
